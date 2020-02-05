42eb46b745bf67b9c5802f239f784b98fadf17ae
Brian Vehige <brianvehige@gmail.com>
2020-02-05 07:55:18 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+require_relative /display_rainbow_spec.rb"
+
+def display_rainbow 
+  
+  prints "R: #{colors[0]},"
+  prints "O: #{colors[1]}," 
+  prints "Y: #{colors[2]},"
+  prints "G: #{colors[3]},"
+  prints "B: #{colors[4]},"
+  prints "I: #{colors[5]},"
+  prints "V: #{colors[6]}"
+  
+end
+
+
